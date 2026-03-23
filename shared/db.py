eaeab59f90fecbd4c93a30912352e8ed614cd839
jeemmy <120758748+jeemmy@users.noreply.github.com>
2026-03-23 02:12:39 +0200

    Update db.py

--- shared/db.py
+++ shared/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:pass@localhost/botdb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://neondb_owner:npg_dvb1T4eNHGRO@ep-lively-sunset-amsgwrp4-pooler.c-5.us-east-1.aws.neon.tech/neondb?ssl=require&channel_binding=require")
 
 engine = create_async_engine(DATABASE_URL, echo=False, pool_size=10, max_overflow=20)
 
